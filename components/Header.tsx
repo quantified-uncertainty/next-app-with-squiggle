edf0ee984e34c96284ca11c06439541ac4203376
Quinn Dougherty <quinnd@riseup.net>
2022-08-01 04:06:55 -0400

    rm comment

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,6 @@
 import styles from "../styles/Header.module.css"
 import Link from "next/link"
 
-// The approach used in this component shows how to build a sign in and sign out
-// component that works on pages which support both client and server side
-// rendering, and avoids any flash incorrect content on initial page load.
 export default function Header() {
 
   return (
